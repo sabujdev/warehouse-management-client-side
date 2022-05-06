8c4d5f2163385001a932deebc3d3e2a4746e2490
sabujmahonto <sabujmahonto@gmail.com>
2022-05-06 10:55:13 +0600

    useProduct hook added

---
+++ src/hooks/useProduct.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+const useProduct = () =>{
+    const [product, setProduct] = useState([]);
+
+    useEffect( ()=>{
+        fetch('http://localhost:5000/product')
+        .then(res => res.json())
+        .then(data => setProduct(data));
+    }, []);
+    return [product, setProduct]
+}
+
+export default useProduct;
